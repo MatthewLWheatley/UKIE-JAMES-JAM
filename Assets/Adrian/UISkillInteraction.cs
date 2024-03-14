bf27b2fa0ee76b55f1abc0f8bfd8e6c83df6d820
adrian haythorne <71699779+adrian-1066@users.noreply.github.com>
2024-03-14 14:27:28 +0000

    added audio stuff

--- Assets/Adrian/UISkillInteraction.cs
+++ Assets/Adrian/UISkillInteraction.cs
@@ -11,16 +11,21 @@ public class UISkillInteraction : MonoBehaviour, IPointerEnterHandler, IPointerE
     //public 
     private Button Self;
     public GameObject ToolTipObj;
+    public AudioManager audioManager;
+    bool SkillActivated;
 
     private void Start()
     {
         Self = GetComponent<Button>();
+        audioManager = GameObject.FindObjectOfType<AudioManager>();
+        SkillActivated = false;
     }
 
     public void ActivateSkill()
     {
         //need to check if currency 
         Self.interactable = false;
+        SkillActivated = true; 
         for(int i = 0; i < NextSkillsInTree.Length; i++)
         {
             NextSkillsInTree[i].interactable = true;
@@ -33,12 +38,21 @@ public class UISkillInteraction : MonoBehaviour, IPointerEnterHandler, IPointerE
     {
         Debug.Log("yo the mouse is over a button babes");
         ToolTipObj.SetActive(true);
+
+        if (!SkillActivated)
+        {
+            audioManager.PlaySFX("ToolTipOn");
+        }
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         Debug.Log("yo the mouse has left the button :(");
         ToolTipObj.SetActive(false);
+        if (!SkillActivated)
+        {
+            audioManager.PlaySFX("ToolTipOff");
+        }
     }
    
 }
